6bffd39e4dd3b9860113d4d9f33ef81ad1a048ab
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 16:29:44 +0200

    app.py

---
+++ src/app.py
@@ -0,0 +1,30 @@
+import pygame
+
+from pysim.graphics.gui.mainwindow import MainWindow
+from pysim.graphics.gui.screen import Screen
+
+
+class TestScreen(Screen):
+    def __init__(self):
+        pass
+
+    def update(self, elapsed_seconds: float) -> None:
+        pass
+
+    def render(self, surface: pygame.Surface) -> None:
+        self.__clear_screen(surface)
+
+    def __clear_screen(self, surface: pygame.Surface) -> None:
+        color = (128, 128, 128)
+        surface.fill(color)
+
+
+def main():
+    pygame.init()
+    screen = TestScreen()
+    window = MainWindow(screen)
+    window.run()
+
+
+if __name__ == '__main__':
+    main()
