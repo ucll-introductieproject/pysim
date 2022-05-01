bfbe26b20fdf86b106e84c007bcf177effef095a
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 19:20:55 +0200

    Particle animation

---
+++ src/pysim/graphics/animations/particle.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+from pygame import Vector2, Rect
+
+from pysim.graphics.animations.animation import Animation
+from pysim.graphics.primitives.primitive import Primitive
+from pysim.graphics.primitives.shapes import Rectangle
+
+
+class Particle(Animation[Primitive]):
+    __position: Vector2
+    __velocity: Vector2
+    __size: float
+    __duration: float
+    __color: Tuple[int, int, int]
+
+    def __init__(self,
+                 position: Vector2,
+                 velocity: Vector2,
+                 size: float,
+                 duration: float,
+                 color: Tuple[int, int, int]):
+        self.__position = position
+        self.__velocity = velocity
+        self.__size = size
+        self.__duration = duration
+        self.__color = color
+
+    @property
+    def duration(self) -> float:
+        return self.__duration
+
+    def __getitem__(self, time: float) -> Primitive:
+        assert 0 <= time < self.__duration
+        current_position = self.__position + time * self.__velocity
+        current_size = self.__size * (1 - time / self.__duration)
+        left = current_position.x - current_size / 2
+        top = current_position.y - current_size / 2
+        rect = Rect(left, top, current_size, current_size)
+        return Rectangle(rect, self.__color)
