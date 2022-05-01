c78a1bbb0dbce917ec018cc4608bdf1390bfbfe4
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 17:30:42 +0200

    LinearFloatAnimation

---
+++ src/pysim/graphics/animations/float.py
@@ -0,0 +1,23 @@
+from .animation import Animation
+
+
+class LinearFloatAnimation(Animation[float]):
+    __start: float
+    __stop: float
+    __duration: float
+
+    def __init__(self, start, stop, duration):
+        assert duration >= 0
+
+        self.__start = start
+        self.__stop = stop
+        self.__duration = duration
+
+    def __getitem__(self, time: float) -> float:
+        assert 0 <= time < self.__duration
+        relative_t = time / self.__duration
+        return self.__start + (self.__stop - self.__start) * relative_t
+
+    @property
+    def duration(self) -> float:
+        return self.__duration
