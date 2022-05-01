332ae642d9305619c083e84fd0ff4e89c0e5d14f
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 17:56:51 +0200

    Animation.map

--- src/pysim/graphics/animations/animation.py
+++ src/pysim/graphics/animations/animation.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, Callable
 
 T = TypeVar('T')
+U = TypeVar('U')
 
 
 class Animation(ABC, Generic[T]):
@@ -13,3 +16,23 @@ class Animation(ABC, Generic[T]):
     @abstractmethod
     def duration(self) -> float:
         ...
+
+    def map(self, transformer: Callable[[T], U]) -> Animation[U]:
+        return _AnimationMapper[T, U](self, transformer)
+
+
+class _AnimationMapper(Generic[T, U], Animation[T]):
+    __animation: Animation[U]
+    __transformer: Callable[[U], T]
+
+    def __init__(self, animation: Animation[U], transformer: Callable[[U], T]):
+        self.__animation = animation
+        self.__transformer = transformer
+
+    @property
+    def duration(self) -> float:
+        return self.__animation.duration
+
+    def __getitem__(self, time: float) -> T:
+        x = self.__animation[time]
+        return self.__transformer(x)
