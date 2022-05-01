058b575f1335432e719eaafe42b0a6a12b91e06d
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 15:10:50 +0200

    Grid + tests

---
+++ src/pysim/data/grid.py
@@ -0,0 +1,23 @@
+from typing import TypeVar, Generic, Callable
+
+from .vector import Vector
+
+T = TypeVar('T')
+
+
+class Grid(Generic[T]):
+    __contents: list[list[T]]
+
+    def __init__(self, width: int, height, initializer: Callable[[Vector], T]):
+        self.__contents = [[initializer(Vector(x, y)) for x in range(width)] for y in range(height)]
+
+    def __getitem__(self, position: Vector) -> T:
+        return self.__contents[position.y][position.x]
+
+    @property
+    def width(self) -> int:
+        return len(self.__contents[0])
+
+    @property
+    def height(self) -> int:
+        return len(self.__contents)
---
+++ tests/grid_tests.py
@@ -0,0 +1,25 @@
+import pytest
+
+from pysim.data.grid import Grid
+from pysim.data.vector import Vector
+
+
+@pytest.mark.parametrize("width, height", [(w, h) for w in range(1, 10) for h in range(1, 10)])
+def test_size(width, height):
+    grid = Grid(width, height, lambda p: None)
+    assert grid.width == width
+    assert grid.height == height
+
+
+@pytest.mark.parametrize("width, height", [(w, h) for w in range(1, 10) for h in range(1, 10)])
+def test_initialization(width, height):
+    def initialize(position: Vector):
+        return position.x + position.y * width
+
+    grid = Grid(width, height, initialize)
+    index = 0
+    for y in range(height):
+        for x in range(width):
+            p = Vector(x, y)
+            assert grid[p] == index
+            index += 1
