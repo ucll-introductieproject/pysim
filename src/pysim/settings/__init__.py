61deebdd6a6708ab6f6df41c5c0579ee3b92dfcc
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 16:17:00 +0200

    global settings

---
+++ src/pysim/settings/__init__.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from .dictionary import DictionarySettings
+from .hierarchy import HierarchicalSettings
+from .settings import Settings
+
+
+class _GlobalSettings:
+    __settings: Settings
+    __runtime_settings: DictionarySettings
+
+    def __init__(self):
+        __runtime_settings = DictionarySettings()
+        self.__settings = HierarchicalSettings([
+            self.__runtime_settings,
+            self.__create_default_settings(),
+        ])
+
+    def __create_default_settings(self) -> Settings:
+        return DictionarySettings({
+            'show_fps': False,
+            'max_fps': 0,
+        })
+
+    def __getitem__(self, key: str) -> Any:
+        return self.__settings[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__runtime_settings[key] = value
+
+
+settings = _GlobalSettings()
+
+__all__ = ['settings']
