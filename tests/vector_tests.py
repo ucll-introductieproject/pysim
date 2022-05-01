496d6ff5a8c417c2251f2d8aeab26db299b89e64
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-01 15:13:57 +0200

    coding style in vector_tests

--- tests/vector_tests.py
+++ tests/vector_tests.py
@@ -1,29 +1,36 @@
-from pysim.data.vector import Vector, NORTH, EAST, SOUTH, WEST
 from pytest import mark
+
+from pysim.data.vector import Vector, NORTH, EAST, SOUTH, WEST
 
 
 @mark.parametrize('x, y', [(x, y) for x in range(-5, 5) for y in range(-5, 5)])
 def test_cw_eq_3ccw(x, y):
     vector = Vector(x, y)
-    assert vector.rotate_clockwise() == vector.rotate_counterclockwise().rotate_counterclockwise().rotate_counterclockwise()
+    cw = vector.rotate_clockwise()
+    ccw3 = vector.rotate_counterclockwise().rotate_counterclockwise().rotate_counterclockwise()
+    assert cw == ccw3
 
 
 @mark.parametrize('x, y', [(x, y) for x in range(-5, 5) for y in range(-5, 5)])
 def test_3cw_eq_ccw(x, y):
     vector = Vector(x, y)
-    assert vector.rotate_clockwise().rotate_clockwise().rotate_clockwise() == vector.rotate_counterclockwise()
+    cw3 = vector.rotate_clockwise().rotate_clockwise().rotate_clockwise()
+    ccw = vector.rotate_counterclockwise()
+    assert cw3 == ccw
 
 
 @mark.parametrize('x, y', [(x, y) for x in range(-5, 5) for y in range(-5, 5)])
 def test_4cw_eq_id(x, y):
     vector = Vector(x, y)
-    assert vector.rotate_clockwise().rotate_clockwise().rotate_clockwise().rotate_clockwise() == vector
+    cw4 = vector.rotate_clockwise().rotate_clockwise().rotate_clockwise().rotate_clockwise()
+    assert cw4 == vector
 
 
 @mark.parametrize('x, y', [(x, y) for x in range(-5, 5) for y in range(-5, 5)])
 def test_4ccw_eq_id(x, y):
     vector = Vector(x, y)
-    assert vector.rotate_counterclockwise().rotate_counterclockwise().rotate_counterclockwise().rotate_counterclockwise() == vector
+    ccw4 = vector.rotate_counterclockwise().rotate_counterclockwise().rotate_counterclockwise().rotate_counterclockwise()
+    assert ccw4 == vector
 
 
 @mark.parametrize('vector, expected', [
