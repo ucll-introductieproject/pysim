be53cec21c7786d1a0e64452e34834cc939957c2
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-02 18:01:48 +0200

    Simulation

---
+++ src/pysim/simulation/simulation.py
@@ -0,0 +1,24 @@
+from pysim.data import Vector
+from pysim.data.orientation import Orientation
+from pysim.simulation.events.event import Event
+from pysim.simulation.events.movement import ForwardEvent, TurnLeftEvent
+
+
+class Simulation:
+    __position: Vector
+    __orientation: Orientation
+
+    def __init__(self, position: Vector, orientation: Orientation):
+        self.__position = position
+        self.__orientation = orientation
+
+    def forward(self) -> Event:
+        old_position = self.__position
+        self.__position += Vector.from_orientation(self.__orientation)
+        event = ForwardEvent(old_position, self.__orientation)
+        return event
+
+    def turn_left(self) -> Event:
+        event = TurnLeftEvent(self.__position, self.__orientation)
+        self.__orientation = self.__orientation.turn_left()
+        return event
