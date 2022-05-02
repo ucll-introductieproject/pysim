6e046849e3527e3523a90fbac28f0f9739944ae5
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-02 22:30:40 +0200

    ParallelEvents

---
+++ src/pysim/simulation/events/parallel.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from pysim.graphics.animations.animation import Animation
+from pysim.graphics.animations.function import FunctionAnimation
+from pysim.graphics.primitives.operations import UnionPrimitive
+from pysim.graphics.primitives.primitive import Primitive
+from pysim.simulation.events.event import Event, AnimationSettings
+
+
+class ParallelEvents(Event):
+    __children: List[Event]
+
+    def __init__(self, children: List[Event]):
+        self.__children = children
+
+    def animate(self, settings: AnimationSettings) -> Animation[Primitive]:
+        def animate(time: float) -> Primitive:
+            return UnionPrimitive([animation[time] for animation in animations])
+
+        animations = [c.animate(settings) for c in self.__children]
+        return FunctionAnimation(1, animate)
