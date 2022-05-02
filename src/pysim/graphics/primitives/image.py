cdc6cda00827a08905dbb32c57375955fbb2be59
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-02 11:26:09 +0200

    Image can be transformed

--- src/pysim/graphics/primitives/image.py
+++ src/pysim/graphics/primitives/image.py
@@ -1,4 +1,4 @@
-from pygame import Surface, Vector2
+from pygame import Surface, Vector2, transform
 
 from pysim.graphics.primitives.primitive import Primitive
 
@@ -15,3 +15,7 @@ class Image(Primitive):
         width, height = self.__surface.get_size()
         position = self.__center - Vector2(width / 2, height / 2)
         surface.blit(self.__surface, position)
+
+    def transform(self, displacement: Vector2, rotation_angle: float) -> Primitive:
+        rotated = transform.rotate(self.__surface, rotation_angle)
+        return Image(self.__center + displacement, rotated)
