decdbbb1c23c3e796399406e396220eb0aa2ec0a
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-02 15:05:09 +0200

    AnimationSettings

--- src/pysim/simulation/events/event.py
+++ src/pysim/simulation/events/event.py
@@ -1,10 +1,24 @@
 from abc import ABC, abstractmethod
 
+from pygame import Rect
+
+from pysim.data import Vector
 from pysim.graphics.animations.animation import Animation
 from pysim.graphics.primitives.primitive import Primitive
 
 
+class AnimationSettings(ABC):
+    @property
+    @abstractmethod
+    def tile_size(self) -> float:
+        ...
+
+    @abstractmethod
+    def tile_rectangle(self, position: Vector) -> Rect:
+        ...
+
+
 class Event(ABC):
     @abstractmethod
-    def animate(self) -> Animation[Primitive]:
+    def animate(self, settings: AnimationSettings) -> Animation[Primitive]:
         ...
--- src/pysim/simulation/events/movement.py
+++ src/pysim/simulation/events/movement.py
@@ -6,7 +6,7 @@ from pysim.graphics.animations.float import LinearFloatAnimation
 from pysim.graphics.animations.function import FunctionAnimation
 from pysim.graphics.primitives.car import create_car
 from pysim.graphics.primitives.primitive import Primitive
-from pysim.simulation.events.event import Event
+from pysim.simulation.events.event import Event, AnimationSettings
 
 
 class ForwardEvent(Event):
@@ -17,23 +17,18 @@ class ForwardEvent(Event):
         self.__start = start
         self.__direction = direction
 
-    def animate(self) -> Animation[Primitive]:
+    def animate(self, settings: AnimationSettings) -> Animation[Primitive]:
         def compute_position(time: float) -> Vector2:
             x = x_anim[time]
             y = y_anim[time]
             return Vector2(x, y)
 
-        wx, wy = self.__start
-        dx, dy = self.__direction
-        tile_size = 128
-        sx = wx * tile_size
-        sy = wy * tile_size
-        ex = (wx + dx) * tile_size
-        ey = (wy + dy) * tile_size
+        sx, sy = settings.tile_rectangle(self.__start).center
+        ex, ey = settings.tile_rectangle(self.__start + self.__direction).center
         x_anim = LinearFloatAnimation(sx, ex, 1)
         y_anim = LinearFloatAnimation(sy, ey, 1)
         pos_anim = FunctionAnimation[Vector2](1, compute_position)
-        canonical_car = create_car(Color(255, 0, 0), tile_size)
+        canonical_car = create_car(Color(255, 0, 0), settings.tile_size)
         return pos_anim.map(lambda p: canonical_car.transform(p, 0))
 
 
@@ -45,12 +40,11 @@ class TurnLeftEvent(Event):
         self.__position = position
         self.__start_angle = start_angle
 
-    def animate(self) -> Animation[Primitive]:
-        tile_size = 128
-        x, y = self.__position
-        position = Vector2(x * tile_size, y * tile_size)
+    def animate(self, settings: AnimationSettings) -> Animation[Primitive]:
+        tile_rect = settings.tile_rectangle(self.__position)
+        position = Vector2(tile_rect.center)
         start = self.__start_angle
         stop = start + 90
         angle_anim = LinearFloatAnimation(start, stop, 1)
-        canonical_car = create_car(Color(255, 0, 0), tile_size)
+        canonical_car = create_car(Color(255, 0, 0), settings.tile_size)
         return angle_anim.map(lambda angle: canonical_car.transform(position, angle))
