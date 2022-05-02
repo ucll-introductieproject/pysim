9499fa3479510391029dc81ae57be8a7428b901b
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-02 16:40:08 +0200

    ForwardEvent gets angle instead of direction vector

--- src/pysim/simulation/events/movement.py
+++ src/pysim/simulation/events/movement.py
@@ -11,11 +11,11 @@ from pysim.simulation.events.event import Event, AnimationSettings
 
 class ForwardEvent(Event):
     __start: Vector
-    __direction: Vector
+    __angle: float
 
-    def __init__(self, start: Vector, direction: Vector):
+    def __init__(self, start: Vector, angle: float):
         self.__start = start
-        self.__direction = direction
+        self.__angle = angle
 
     def animate(self, settings: AnimationSettings) -> Animation[Primitive]:
         def compute_position(time: float) -> Vector2:
@@ -23,13 +23,14 @@ class ForwardEvent(Event):
             y = y_anim[time]
             return Vector2(x, y)
 
+        direction = Vector.from_polar(self.__angle, 1)
         sx, sy = settings.tile_rectangle(self.__start).center
-        ex, ey = settings.tile_rectangle(self.__start + self.__direction).center
+        ex, ey = settings.tile_rectangle(self.__start + direction).center
         x_anim = LinearFloatAnimation(sx, ex, 1)
         y_anim = LinearFloatAnimation(sy, ey, 1)
         pos_anim = FunctionAnimation[Vector2](1, compute_position)
         canonical_car = create_car(Color(255, 0, 0), settings.tile_size)
-        return pos_anim.map(lambda p: canonical_car.transform(p, 0))
+        return pos_anim.map(lambda p: canonical_car.transform(p, self.__angle))
 
 
 class TurnLeftEvent(Event):
@@ -44,7 +45,7 @@ class TurnLeftEvent(Event):
         tile_rect = settings.tile_rectangle(self.__position)
         position = Vector2(tile_rect.center)
         start = self.__start_angle
-        stop = start + 90
+        stop = start - 90
         angle_anim = LinearFloatAnimation(start, stop, 1)
         canonical_car = create_car(Color(255, 0, 0), settings.tile_size)
         return angle_anim.map(lambda angle: canonical_car.transform(position, angle))
