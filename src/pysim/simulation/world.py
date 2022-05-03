d7bc3acab36e064ecd6d5ee850565b153d6b0957
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-03 11:18:32 +0200

    tiles and world render themselves

--- src/pysim/simulation/world.py
+++ src/pysim/simulation/world.py
@@ -1,18 +1,33 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from pygame import Rect, Surface, draw
 
 from pysim.data import Grid, Vector
+from pysim.simulation.events.event import AnimationSettings
 
 
 class Tile(ABC):
-    pass
+    @abstractmethod
+    def render(self, surface: Surface, rect: Rect) -> None:
+        ...
 
 
 class Empty(Tile):
-    pass
+    def render(self, surface: Surface, rect: Rect) -> None:
+        color = (255, 255, 255)
+        draw.rect(surface, color, rect)
 
 
 class Wall(Tile):
-    pass
+    def render(self, surface: Surface, rect: Rect) -> None:
+        color = (64, 64, 64)
+        draw.rect(surface, color, rect)
+
+
+class Chasm(Tile):
+    def render(self, surface: Surface, rect: Rect) -> None:
+        color = (0, 0, 255)
+        draw.rect(surface, color, rect)
 
 
 class World:
@@ -31,3 +46,14 @@ class World:
 
     def __getitem__(self, position: Vector) -> Tile:
         return self.__grid[position]
+
+    def render(self, surface: Surface, settings: AnimationSettings) -> None:
+        for y in range(self.width):
+            for x in range(self.height):
+                position = Vector(x, y)
+                self.__render_tile(surface, settings, position)
+
+    def __render_tile(self, surface: Surface, settings: AnimationSettings, position: Vector) -> None:
+        rect = settings.tile_rectangle(position)
+        tile = self[position]
+        tile.render(surface, rect)
