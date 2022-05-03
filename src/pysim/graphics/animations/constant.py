3e55bf6d0597f346c7dd269802b317a477294c4c
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-03 12:09:56 +0200

    ConstantAnimation

---
+++ src/pysim/graphics/animations/constant.py
@@ -0,0 +1,24 @@
+from typing import TypeVar
+
+from .animation import Animation
+
+T = TypeVar('T')
+
+
+class ConstantAnimation(Animation[float]):
+    __constant: T
+    __duration: float
+
+    def __init__(self, constant: T, duration: float):
+        assert duration >= 0
+
+        self.__constant = constant
+        self.__duration = duration
+
+    def __getitem__(self, time: float) -> float:
+        assert 0 <= time < self.__duration
+        return self.__constant
+
+    @property
+    def duration(self) -> float:
+        return self.__duration
