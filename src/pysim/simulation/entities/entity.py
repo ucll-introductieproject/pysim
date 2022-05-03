b05239a68b1f2fa2202c0708ad629a9db2ab5885
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-03 23:22:59 +0200

    Entity stay abstract method

--- src/pysim/simulation/entities/entity.py
+++ src/pysim/simulation/entities/entity.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
 
 from pysim.data import Vector
+from pysim.simulation.events.event import Event
 
 
 class Entity(ABC):
     @property
     @abstractmethod
     def position(self) -> Vector:
+        ...
+
+    @abstractmethod
+    def stay(self) -> Event:
         ...
