d18d717bbd7c677e8edea2597de399e263b9566c
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-03 19:46:03 +0200

    Simulation can push blocks

--- src/pysim/simulation/simulation.py
+++ src/pysim/simulation/simulation.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
-from typing import List, Tuple
+from typing import List, Tuple, cast
 
+from pysim.data import Vector
 from pysim.simulation.entities.agent import Agent, BumpEvent
+from pysim.simulation.entities.block import Block
+from pysim.simulation.entities.entity import Entity
 from pysim.simulation.events.event import Event
 from pysim.simulation.events.parallel import ParallelEvents
 from pysim.simulation.world import World, Wall
@@ -11,10 +14,12 @@ from pysim.simulation.world import World, Wall
 class Simulation:
     __world: World
     __agent: Agent
+    __entities: List[Entity]
 
-    def __init__(self, world: World, agent: Agent):
+    def __init__(self, world: World, agent: Agent, entities: List[Entity]):
         self.__world = world
         self.__agent = agent
+        self.__entities = entities
 
     @property
     def world(self) -> World:
@@ -24,17 +29,33 @@ class Simulation:
     def agent(self) -> Agent:
         return self.__agent
 
+    @property
+    def entities(self) -> List[Entity]:
+        return self.__entities
+
     def forward(self) -> Tuple[Simulation, Event]:
         world = self.world
         agent = self.agent
-        new_agent, event = agent.forward()
-        destination_tile = world[new_agent.position]
-        if isinstance(destination_tile, Wall):
+        new_agent, agent_event = agent.forward()
+        destination = new_agent.position
+        if self.__contains_wall(destination):
             bump_event = BumpEvent(agent.position, agent.orientation)
             return (self, bump_event)
+        elif self.__contains_block(destination):
+            position_beyond_block = agent.position + Vector.from_orientation(agent.orientation) * 2
+            if self.__is_free(position_beyond_block):
+                block = cast(Block, self.__get_entities_at(destination)[0])
+                new_block, block_event = block.move(agent.orientation)
+                new_entities = self.__replace_entity(self.__entities, block, new_block)
+                new_state = Simulation(world, new_agent, new_entities)
+                combined_event = ParallelEvents([agent_event, block_event])
+                return (new_state, combined_event)
+            else:
+                bump_event = BumpEvent(agent.position, agent.orientation)
+                return (self, bump_event)
         else:
-            new_state = Simulation(world, new_agent)
-            return (new_state, event)
+            new_state = Simulation(world, new_agent, self.__entities)
+            return (new_state, agent_event)
 
     def backward(self) -> Tuple[Simulation, Event]:
         world = self.world
@@ -45,21 +66,21 @@ class Simulation:
             bump_event = BumpEvent(agent.position, agent.orientation)
             return (self, bump_event)
         else:
-            new_state = Simulation(world, new_agent)
+            new_state = Simulation(world, new_agent, self.__entities)
             return (new_state, event)
 
     def turn_left(self) -> Tuple[Simulation, Event]:
         world = self.world
         agent = self.agent
         new_agent, event = agent.turn_left()
-        new_state = Simulation(world, new_agent)
+        new_state = Simulation(world, new_agent, self.__entities)
         return (new_state, event)
 
     def turn_right(self) -> Tuple[Simulation, Event]:
         world = self.world
         agent = self.agent
         new_agent, event = agent.turn_right()
-        new_state = Simulation(world, new_agent)
+        new_state = Simulation(world, new_agent, self.__entities)
         return (new_state, event)
 
     def __pack_events(self, events: List[Event]) -> Event:
@@ -67,6 +88,29 @@ class Simulation:
             return events[0]
         else:
             return ParallelEvents(events)
+
+    def __contains_wall(self, position: Vector) -> bool:
+        return isinstance(self.__world[position], Wall)
+
+    def __contains_block(self, position: Vector) -> bool:
+        return any(e.position == position and isinstance(e, Block) for e in self.__entities)
+
+    def __is_free(self, position: Vector) -> bool:
+        '''
+        Checks if there are no entities at the given position
+        :param position: Position to check
+        :return: True if no objects are present, False otherwise
+        '''
+        return not any(e.position == position for e in self.__entities)
+
+    def __get_entities_at(self, position: Vector) -> List[Entity]:
+        return [e for e in self.__entities if e.position == position]
+
+    def __replace_entity(self, entities: List[Entity], entity: Entity, new_entity: Entity) -> List[Entity]:
+        return [
+            new_entity,
+            *(e for e in self.__entities if e is not entity)
+        ]
 
 
 class Simulator:
