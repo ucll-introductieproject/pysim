9044e688c04fdba670e735f414e9703675bea35a
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-07 12:54:44 +0200

    extra assertions in Grid

--- src/pysim/data/grid.py
+++ src/pysim/data/grid.py
@@ -9,9 +9,13 @@ class Grid(Generic[T]):
     __contents: list[list[T]]
 
     def __init__(self, width: int, height, initializer: Callable[[Vector], T]):
+        assert width >= 1
+        assert height >= 1
         self.__contents = [[initializer(Vector(x, y)) for x in range(width)] for y in range(height)]
 
     def __getitem__(self, position: Vector) -> T:
+        assert 0 <= position.x < self.width, f'{position.x} not between 0 and {self.width}'
+        assert 0 <= position.y < self.height, f'{position.y} not between 0 and {self.height}'
         return self.__contents[position.y][position.x]
 
     @property
