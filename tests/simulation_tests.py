c916784243930bd722c07d0ac4bf945f6d312dcc
Frederic Vogels <frederic.vogels@ucll.be>
2022-05-07 14:43:12 +0200

    refactored simulation tests

--- tests/simulation_tests.py
+++ tests/simulation_tests.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from pytest import mark
+
 from pysim.data import Grid, Vector
 from pysim.data.orientation import NORTH, EAST, WEST, SOUTH
 from pysim.simulation.entities.agent import Agent
@@ -6,7 +10,7 @@ from pysim.simulation.tiles import Tile, Empty, Wall, Chasm
 from pysim.simulation.world import World
 
 
-def parse_world(s: str) -> World:
+def parse_world(rows: List[str]) -> World:
     def initialize(position: Vector) -> Tile:
         x, y = position
         match tile := rows[y][x]:
@@ -19,57 +23,52 @@ def parse_world(s: str) -> World:
             case _:
                 assert False, f'unparseable {tile}'
 
-    rows = [row.strip() for row in s.strip().split("\n")]
     width = len(rows[0])
     height = len(rows)
     return World(Grid(width, height, initialize))
 
 
-def test_forward_east():
-    world = parse_world('''
-    ..
-    ''')
-    agent = Agent(Vector(0, 0), EAST)
+@mark.parametrize("world, start_position, start_orientation, end_position", [
+    (
+            [
+                '..',
+            ],
+            (0, 0),
+            EAST,
+            (1, 0)
+    ),
+    (
+            [
+                '..',
+            ],
+            (1, 0),
+            WEST,
+            (0, 0)
+    ),
+    (
+            [
+                '.',
+                '.',
+            ],
+            (0, 0),
+            SOUTH,
+            (0, 1)
+    ),
+    (
+            [
+                '.',
+                '.',
+            ],
+            (0, 1),
+            NORTH,
+            (0, 0)
+    ),
+])
+def test_forward(world, start_position, start_orientation, end_position):
+    world = parse_world(world)
+    agent = Agent(Vector(*start_position), start_orientation)
     entities = []
     simulation = Simulation(world, agent, entities)
     result, event = simulation.forward()
-    assert result.agent.position == Vector(1, 0)
-    assert result.agent.orientation is EAST
-
-
-def test_forward_north():
-    world = parse_world('''
-        .
-        .
-        ''')
-    agent = Agent(Vector(0, 1), NORTH)
-    entities = []
-    simulation = Simulation(world, agent, entities)
-    result, event = simulation.forward()
-    assert result.agent.position == Vector(0, 0)
-    assert result.agent.orientation is NORTH
-
-
-def test_forward_west():
-    world = parse_world('''
-        ..
-        ''')
-    agent = Agent(Vector(1, 0), WEST)
-    entities = []
-    simulation = Simulation(world, agent, entities)
-    result, event = simulation.forward()
-    assert result.agent.position == Vector(0, 0)
-    assert result.agent.orientation is WEST
-
-
-def test_forward_south():
-    world = parse_world('''
-        .
-        .
-        ''')
-    agent = Agent(Vector(0, 0), SOUTH)
-    entities = []
-    simulation = Simulation(world, agent, entities)
-    result, event = simulation.forward()
-    assert result.agent.position == Vector(0, 1)
-    assert result.agent.orientation is SOUTH
+    assert result.agent.position == Vector(*end_position)
+    assert result.agent.orientation is start_orientation
