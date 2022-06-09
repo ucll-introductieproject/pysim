5a059ee1c39fd42dd5da1446de16dc396666c270
Frederic Vogels <frederic.vogels@ucll.be>
2022-06-09 15:26:21 +0200

    forward() pushes objects

--- src/pysim/simulation/simulation.py
+++ src/pysim/simulation/simulation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
+from pysim.data import Vector
 from pysim.simulation.world import World
 
 
@@ -14,10 +15,18 @@ class Simulation:
 
     def forward(self, agent_index: int) -> None:
         agent = self.__world.agents[agent_index]
-        destination = agent.forward_destination()
+        destination = agent.position + Vector.from_orientation(agent.orientation)
         tile_at_destination = self.__world[destination]
         if tile_at_destination.is_passable():
-            agent.forward()
+            if tile_at_destination.contents is not None:
+                object_push_destination = agent.position + Vector.from_orientation(agent.orientation) * 2
+                tile_receiving_object = self.__world[object_push_destination]
+                if tile_receiving_object.accepts_objects and tile_receiving_object.contents is None:
+                    tile_at_destination.contents = None
+                    tile_receiving_object.contents = tile_at_destination.contents
+                    agent.forward()
+            else:
+                agent.forward()
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Simulation):
