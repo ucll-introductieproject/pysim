26c48a06faf6c9d26f0613707469b65832b899e4
Frederic Vogels <frederic.vogels@ucll.be>
2022-06-26 19:05:10 +0200

    Grid.positions

--- src/pysim/data/grid.py
+++ src/pysim/data/grid.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from copy import deepcopy
-from typing import TypeVar, Generic, Callable, Any
+from typing import TypeVar, Generic, Callable, Any, Iterable
 
 from .vector import Vector
 
@@ -35,12 +35,18 @@ class Grid(Generic[T]):
     def height(self) -> int:
         return len(self.__contents)
 
+    @property
+    def positions(self) -> Iterable[Vector]:
+        for y in range(self.height):
+            for x in range(self.width):
+                yield Vector(x, y)
+
     def __copy__(self) -> Grid[T]:
         return Grid(self.width, self.height, self.__getitem__)
 
-    def __deepcopy__(self, memodict: Any) -> Grid[T]:
+    def __deepcopy__(self, memo: Any) -> Grid[T]:
         def copy_item(position: Vector) -> T:
-            return deepcopy(self[position], memodict)
+            return deepcopy(self[position], memo)
 
         return Grid(self.width, self.height, copy_item)
 
