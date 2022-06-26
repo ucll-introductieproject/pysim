e4a636176ac030da49268af06ba7741581b5b4a2
Frederic Vogels <frederic.vogels@ucll.be>
2022-06-26 19:08:59 +0200

    updated Block's MovedEvent

--- src/pysim/simulation/entities/block.py
+++ src/pysim/simulation/entities/block.py
@@ -4,14 +4,18 @@ from pysim.simulation.entities.entity import Entity
 from pysim.simulation.events.event import Event
 
 
-# TODO Add fields
 class _MovedEvent(Event):
-    pass
+    __position: Vector
+    __direction: Orientation
+
+    def __init__(self, position: Vector, direction: Orientation):
+        self.__position = position
+        self.__direction = direction
 
 
 class Block(Entity):
     def move(self, position: Vector, direction: Orientation) -> Event:
-        return _MovedEvent()
+        return _MovedEvent(position, direction)
 
     def is_movable(self) -> bool:
         return True
