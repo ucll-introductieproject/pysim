e1a2f5aa74f32f37d3bc87228097414637e930ce
Frederic Vogels <frederic.vogels@ucll.be>
2022-06-26 19:11:38 +0200

    Updated Simulation

--- src/pysim/simulation/simulation.py
+++ src/pysim/simulation/simulation.py
@@ -1,48 +1,49 @@
 from __future__ import annotations
 
-from typing import Any, TypeVar, Generic
+from typing import Any
 
-from pysim.data import Vector
-from pysim.simulation.events.eventfactory import EventFactory
+from pysim.simulation.agent import Agent
+from pysim.simulation.events import Event
 from pysim.simulation.world import World
 
-Event = TypeVar('Event')
 
-
-class Simulation(Generic[Event]):
+class Simulation:
     __world: World
-    __event_factory: EventFactory[Event]
 
-    def __init__(self, world: World, event_factory: EventFactory[Event]) -> None:
+    def __init__(self, world: World) -> None:
         assert world is not None
         self.__world = world
-        self.__event_factory = event_factory
 
     def forward(self, agent_index: int) -> Event:
-        ef = self.__event_factory
-        agent = self.__world.agents[agent_index]
-        agent_destination = agent.position + Vector.from_orientation(agent.orientation)
-        tile_at_destination = self.__world[agent_destination]
-        if tile_at_destination.is_passable():
-            if tile_at_destination.contents is not None:
-                object_origin = agent_destination
-                object_destination = agent.position + Vector.from_orientation(agent.orientation) * 2
-                tile_receiving_object = self.__world[object_destination]
-                if tile_receiving_object.accepts_objects:
-                    tile_at_destination.contents = None
-                    tile_receiving_object.contents = tile_at_destination.contents
-                    agent.forward()
-                    return ef.parallel(
-                        ef.actor_moved_forward(agent_index),
-                        ef.object_moved(object_origin, agent.orientation)
-                    )
+        agent_position = self.__world.agent_positions[agent_index]
+        agent = self.__world[agent_position].contents
+        assert isinstance(agent, Agent)
+        agent_destination = agent_position.move(agent.orientation)
+        destination_tile = self.__world[agent_destination]
+        if destination_tile.is_traversable():
+            entity = destination_tile.contents
+            if entity is not None:
+                if entity.is_movable():
+                    push_direction = agent.orientation
+                    entity_position = agent_destination
+                    entity_destination = agent_position.move(agent.orientation, distance=2)
+                    tile_receiving_object = self.__world[entity_destination]
+                    if tile_receiving_object.accepts_objects:
+                        destination_tile.contents = None
+                        tile_receiving_object.contents = destination_tile.contents
+                        forward_event = agent.forward(agent_position)
+                        self.__world.agent_positions[agent_index] = agent_destination
+                        move_event = entity.move(entity_position, push_direction)
+                        return forward_event.parallel_with(move_event)
+                    else:
+                        return Event.zero()
                 else:
-                    return ef.nothing()
+                    return Event.zero()
             else:
-                agent.forward()
-                return ef.actor_moved_forward(agent_index)
+                self.__world.agent_positions[agent_index] = agent_destination
+                return agent.forward(agent_position)
         else:
-            return ef.nothing()
+            return Event.zero()
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Simulation):
